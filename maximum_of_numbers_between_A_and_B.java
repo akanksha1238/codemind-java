24606ce4556f226e25e885721cb038379d288710
akanksha1238 <84381942+akanksha1238@users.noreply.github.com>
2022-10-03 19:12:37 +0530

    import java.util.*; class Sample {     public static void main(String args[])throws Exception     {         int n,a,b,c=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         int max=-1;         for(int j=0;j<n;j++)         {             if(arr[j]>=a && arr[j]<=b)             {                if(arr[j]>max)                max=arr[j];             }         }         System.out.print(max);     } }

---
+++ maximum_of_numbers_between_A_and_B.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])throws Exception
+    {
+        int n,a,b,c=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int max=-1;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]>=a && arr[j]<=b)
+            {
+               if(arr[j]>max)
+               max=arr[j];
+            }
+        }
+        System.out.print(max);
+    }
+}
