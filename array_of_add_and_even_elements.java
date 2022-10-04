12ba134c1abdbef25ae90386a8f037902f1b214a
akanksha1238 <84381942+akanksha1238@users.noreply.github.com>
2022-10-04 20:16:32 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int j=0;j<n;j++)         {             if(arr[j]%2==1)             System.out.print(arr[j]+" ");         }         for(int j=0;j<n;j++)         {             if(arr[j]%2==0)             System.out.print(arr[j]+" ");         }     } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]%2==1)
+            System.out.print(arr[j]+" ");
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]%2==0)
+            System.out.print(arr[j]+" ");
+        }
+    }
+}
