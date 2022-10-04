1f284101a8fea1112f1832cefc75a2f25b3ffe8d
akanksha1238 <84381942+akanksha1238@users.noreply.github.com>
2022-10-04 21:39:13 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         int n,avg,s=0,c=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int j=n-1;j>=0;j--)         {             if(arr[j]%2==0)             {                 System.out.print(arr[j]);                 break;             }         }     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        int n,avg,s=0,c=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int j=n-1;j>=0;j--)
+        {
+            if(arr[j]%2==0)
+            {
+                System.out.print(arr[j]);
+                break;
+            }
+        }
+    }
+}
