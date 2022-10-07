6b1ab9592afad724eeda1fdb54f56c4e05006478
akanksha1238 <84381942+akanksha1238@users.noreply.github.com>
2022-10-07 18:01:16 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         int t;         Scanner sc=new Scanner(System.in);         t=sc.nextInt();         for(int i=0;i<t;i++)         {             int a,b;             a=sc.nextInt();             b=sc.nextInt();             System.out.println(a+b);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        int t;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int a,b;
+            a=sc.nextInt();
+            b=sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
